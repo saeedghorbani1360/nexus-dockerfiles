2692ca9fc2420c3654918c77cffeaf5fe690d84b
Saeed Ghorbani <saeed_qorbani@yahoo.com>
2024-03-26 21:08:26 +0000

    add script

---
+++ container_backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Backup from running containers"
+
+cons=`docker ps --format "{{.Names}}"`
+date=`date +%Y-%m-%d_%H-%M-%S`
+
+find . -name "backup_*.tar" -mmin +600 -exec rm -rf {} \;
+
+for i in $cons
+do
+	echo "Container name : " $i
+	newname="backup_${i}:${date}"
+	docker commit ${i} ${newname} >> /dev/null 
+	docker save ${newname} -o ${newname}.tar
+done
+
